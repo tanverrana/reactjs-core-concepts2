8892525dd9b11d49cbb28ad48e5b511152df3809
tanverrana <tanversobur@gmail.com>
2022-03-16 10:57:33 +0600

    comment procucts

--- src/App.js
+++ src/App.js
@@ -2,23 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const products = [
+
+
+  return (
+    <div className="App" >
+
+    </div>
+  );
+}
+
+/* 
+const products = [
     { name: "Lenovo", price: "73893" },
     { name: "MacBook", price: "733493" },
     { name: "HP", price: "7893" },
     { name: "ACER", price: "3893" },
   ];
-
-  return (
-    <div className="App" >
-      {
-        products.map(product => <Product name={product.name} price={product.price}></Product>)
-      }
-      <Product name="Laptop" price="34000"></Product>
-      <Product name="Mobile" price="4000"></Product>
-    </div>
-  );
+*/
+/* {
+  products.map(product => <Product name={product.name} price={product.price}></Product>)
 }
+<Product name="Laptop" price="34000"></Product>
+<Product name="Mobile" price="4000"></Product> 
+*/
 function Product(props) {
   return (
     <div className="product">
