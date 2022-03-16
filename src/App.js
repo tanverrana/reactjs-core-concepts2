481a957d03d433f239ac8519ef3602aea8034bc3
tanverrana <tanversobur@gmail.com>
2022-03-16 12:08:04 +0600

    api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
 
@@ -13,9 +14,27 @@ function App() {
   );
 }
 function Externalusers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
   return (
     <div>
       <h2>External User</h2>
+      <h3>{users.length}</h3>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+};
+function User(props) {
+  return (
+    <div>
+      <h4>Name:{props.name}</h4>
+      <h4>Email:{props.email}</h4>
     </div>
   )
 }
