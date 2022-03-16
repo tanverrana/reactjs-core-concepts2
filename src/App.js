b499384a2a62a84a92b1e49d500cb2a42ec760af
tanverrana <tanversobur@gmail.com>
2022-03-16 11:18:13 +0600

    decrase button

--- src/App.js
+++ src/App.js
@@ -14,14 +14,20 @@ function App() {
 
 function Counter() {
   const [count, setCount] = useState(55);
-  const increaseCount = () => {
-    const newCount = count + 1;
-    setCount(newCount);
-  }
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+
+  /* function Counter() {
+    const [count, setCount] = useState(55);
+    const increaseCount = () => {
+      const newCount = count + 1;
+      setCount(newCount);
+    } */
   return (
     <div>
       <h1>Count:{count}</h1>
       <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
     </div>
   )
 }
